f0d62af0dd8fe8ef1d1439485439bd708519e24a
charlesmarshall <cm56marshall@googlemail.com>
2012-04-16 20:46:48 +0100

    new client controller

---
+++ resources/app/controller/admin/AdminClientController.php
@@ -0,0 +1,9 @@
+<?
+class AdminClientController extends AdminComponent{
+
+  public $module_name = "client";
+  public $model_class = 'Client';
+  public $display_name = "Clients";
+  public $dashboard = false;
+}
+?>
