c03699dfaacebe264a12043d633836be87860975
charlesmarshall <cm56marshall@googlemail.com>
2012-04-16 20:52:31 +0100

    Organisation cms contorller

---
+++ resources/app/controller/admin/AdminOrganisationController.php
@@ -0,0 +1,9 @@
+<?
+class AdminOrganisationController extends AdminComponent{
+
+  public $module_name = "organisation";
+  public $model_class = 'Organisation';
+  public $display_name = "Organisations";
+  public $dashboard = false;
+}
+?>
--- setup.php
+++ setup.php
@@ -4,4 +4,6 @@ CMSApplication::register_module("jobcomment", array("display_name"=>"Comments on
 CMSApplication::register_module("department", array("display_name"=>"Departments", "link"=>"/admin/department/"));
 CMSApplication::register_module("fee", array("display_name"=>"Fees", "link"=>"/admin/fee/"));
 CMSApplication::register_module("job", array("display_name"=>"Jobs", "link"=>"/admin/job/"));
+CMSApplication::register_module("organisation", array("display_name"=>"Organisations", "link"=>"/admin/organisation/"));
+
 ?>
