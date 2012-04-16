2aa4f851a809e5e842f9a7b4c831a34b9021f7c4
charlesmarshall <cm56marshall@googlemail.com>
2012-04-16 20:46:29 +0100

    ini & setup files

---
+++ setup.php
@@ -0,0 +1,4 @@
+<?
+CMSApplication::register_module("client", array('plugin_name'=>'wildfire.resources', 'assets_for_cms'=>true, "display_name"=>"Clients", "link"=>"/admin/client/", 'split'=>true));
+
+?>
