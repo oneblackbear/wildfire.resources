9264c038fca380975318a65d4f9caaf9e1cae20d
charlesmarshall <cm56marshall@googlemail.com>
2012-04-16 20:48:40 +0100

    comments cms module

---
+++ resources/app/controller/admin/AdminJobcommentController.php
@@ -0,0 +1,9 @@
+<?
+class AdminJobcommentController extends AdminComponent{
+
+  public $module_name = "jobcomment";
+  public $model_class = 'Comment';
+  public $display_name = "Comments on jobs";
+  public $dashboard = false;
+}
+?>
--- setup.php
+++ setup.php
@@ -1,4 +1,4 @@
 <?
-CMSApplication::register_module("client", array('plugin_name'=>'wildfire.resources', 'assets_for_cms'=>true, "display_name"=>"Clients", "link"=>"/admin/client/", 'split'=>true));
-
+CMSApplication::register_module("client", array('plugin_name'=>'wildfire.resources', 'assets_for_cms'=>true, "display_name"=>"Clients", "link"=>"/admin/client/"));
+CMSApplication::register_module("jobcomment", array("display_name"=>"Comments on jobs", "link"=>"/admin/jobcomment/"));
 ?>
