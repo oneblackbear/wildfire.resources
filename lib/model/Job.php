1097f32fa195a9cc635203fb16926ccb5f50cde5
charlesmarshall <cm56marshall@googlemail.com>
2012-04-16 20:13:45 +0100

    job model

---
+++ lib/model/Job.php
@@ -0,0 +1,12 @@
+<?
+class Job extends WildfireResource{
+
+  public function setup(){
+    parent::setup();
+    $this->define("date_internal_testing", "DateTimeField", array('label'=>'Internal testing date'));
+    $this->define("date_client_testing", "DateTimeField", array('label'=>'Client testing date'));
+    $this->define("date_go_live", "DateTimeField", array('label'=>'Go live date'));
+  }
+
+}
+?>
