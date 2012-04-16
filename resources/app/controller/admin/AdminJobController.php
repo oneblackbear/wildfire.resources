349fa46770af2a47447177d4983694c854ff1085
charlesmarshall <cm56marshall@googlemail.com>
2012-04-16 20:51:29 +0100

    cms controller for jobs

---
+++ resources/app/controller/admin/AdminJobController.php
@@ -0,0 +1,9 @@
+<?
+class AdminJobController extends AdminComponent{
+
+  public $module_name = "job";
+  public $model_class = 'Job';
+  public $display_name = "Jobs";
+  public $dashboard = false;
+}
+?>
--- setup.php
+++ setup.php
@@ -3,4 +3,5 @@ CMSApplication::register_module("client", array('plugin_name'=>'wildfire.resourc
 CMSApplication::register_module("jobcomment", array("display_name"=>"Comments on jobs", "link"=>"/admin/jobcomment/"));
 CMSApplication::register_module("department", array("display_name"=>"Departments", "link"=>"/admin/department/"));
 CMSApplication::register_module("fee", array("display_name"=>"Fees", "link"=>"/admin/fee/"));
+CMSApplication::register_module("job", array("display_name"=>"Jobs", "link"=>"/admin/job/"));
 ?>
