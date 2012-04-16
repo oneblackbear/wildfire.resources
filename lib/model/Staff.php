9b0e66ac165f3f9a35278205fa9042df836950f0
charlesmarshall <cm56marshall@googlemail.com>
2012-04-16 21:58:06 +0100

    staff model tweaks

--- lib/model/Staff.php
+++ lib/model/Staff.php
@@ -1,6 +1,5 @@
 <?
-//do admin by staff being a member of a 'super admin' user group - they can then do scheduling etc for the dept. they are in
-//add in passwords, email, usernames & general contact details
+//some form of permissions will be needed...
 class Staff extends WildfireResource{
   public static $days_of_week = array('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday');
   public function setup(){
@@ -9,7 +8,12 @@ class Staff extends WildfireResource{
     $this->define("organisations", "ManyToManyField", array('target_model'=>"Organisation", 'group'=>'relationships'));
     $this->define("clients", "ManyToManyField", array('target_model'=>"Client", 'group'=>'relationships'));
     foreach(self::$days_of_week as $day) $this->define("hours_on_".$day, "IntegerField", array('maxlength'=>2, 'default'=>0));
+    $this->define("telephone", "CharField", array('scaffold'=>true, 'export'=>true));
+    $this->define("email", "CharField", array('scaffold'=>true, 'export'=>true));
+    $this->define("password", "PasswordField", array('label'=>'Enter your password', 'group'=>'password'));
   }
+
+
 
 }
 ?>
--- resources/app/controller/admin/AdminStaffController.php
+++ resources/app/controller/admin/AdminStaffController.php
@@ -6,7 +6,7 @@ class AdminStaffController extends AdminComponent{
   public $display_name = "Staff";
   public $dashboard = false;
   public $filter_fields=array(
-                          'text' => array('columns'=>array('id', 'title', 'content'), 'partial'=>'_filters_text', 'fuzzy'=>true),
+                          'text' => array('columns'=>array('id', 'title', 'email'), 'partial'=>'_filters_text', 'fuzzy'=>true),
                           'departments' => array('columns'=>array('departments'), 'partial'=>'_filters_select', 'opposite_join_column'=>'staff'),
                           'organisations' => array('columns'=>array('organisations'), 'partial'=>'_filters_select', 'opposite_join_column'=>'staff')
                         );
