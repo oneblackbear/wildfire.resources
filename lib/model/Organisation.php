05b2c355adcc74907b8c71b5794cc316c1a6ce3f
charlesmarshall <cm56marshall@googlemail.com>
2012-04-16 20:06:55 +0100

    join to staff

--- lib/model/Organisation.php
+++ lib/model/Organisation.php
@@ -1,6 +1,5 @@
 <?
 class Organisation extends WildfireResource{
-
   public function setup(){
     parent::setup();
     $this->define("departments", "ManyToManyField", array('target_model'=>"Department", 'group'=>'relationships'));
