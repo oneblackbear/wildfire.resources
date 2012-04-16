d1d28e260091e5c6c1b50616dc3e7ccbaa4a746d
charlesmarshall <cm56marshall@googlemail.com>
2012-04-16 20:07:27 +0100

    basic time per day allocation

--- lib/model/Staff.php
+++ lib/model/Staff.php
@@ -1,10 +1,11 @@
 <?
 class Staff extends WildfireResource{
-
+  public static $days_of_week = array('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday');
   public function setup(){
     parent::setup();
     $this->define("departments", "ManyToManyField", array('target_model'=>"Department", 'group'=>'relationships'));
     $this->define("organisations", "ManyToManyField", array('target_model'=>"Organisation", 'group'=>'relationships'));
+    foreach(self::$days_of_week as $day) $this->define("hours_on_".$day, "IntegerField", array('maxlength'=>2, 'default'=>0));
   }
 
 
