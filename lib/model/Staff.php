607c398ad91623eac84eef14b9460c69a8382ff4
charlesmarshall <cm56marshall@googlemail.com>
2012-04-16 21:21:54 +0100

    add to scaffold

--- lib/model/Staff.php
+++ lib/model/Staff.php
@@ -5,7 +5,7 @@ class Staff extends WildfireResource{
   public static $days_of_week = array('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday');
   public function setup(){
     parent::setup();
-    $this->define("departments", "ManyToManyField", array('target_model'=>"Department", 'group'=>'relationships'));
+    $this->define("departments", "ManyToManyField", array('target_model'=>"Department", 'group'=>'relationships', 'scaffold'=>true));
     $this->define("organisations", "ManyToManyField", array('target_model'=>"Organisation", 'group'=>'relationships'));
     $this->define("clients", "ManyToManyField", array('target_model'=>"Client", 'group'=>'relationships'));
     foreach(self::$days_of_week as $day) $this->define("hours_on_".$day, "IntegerField", array('maxlength'=>2, 'default'=>0));
