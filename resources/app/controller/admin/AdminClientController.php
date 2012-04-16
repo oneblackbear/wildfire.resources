d8a326cc7021fa03466edf7909cba25f4742cef9
charlesmarshall <cm56marshall@googlemail.com>
2012-04-16 21:31:30 +0100

    improve filters

--- resources/app/controller/admin/AdminClientController.php
+++ resources/app/controller/admin/AdminClientController.php
@@ -5,5 +5,9 @@ class AdminClientController extends AdminComponent{
   public $model_class = 'Client';
   public $display_name = "Clients";
   public $dashboard = false;
+  public $filter_fields=array(
+                          'text' => array('columns'=>array('id', 'title', 'content'), 'partial'=>'_filters_text', 'fuzzy'=>true),
+                          'account_handler' => array('columns'=>array('account_handler'), 'partial'=>'_filters_select', 'opposite_join_column'=>'clients')
+                        );
 }
 ?>
--- resources/app/controller/admin/AdminStaffController.php
+++ resources/app/controller/admin/AdminStaffController.php
@@ -5,5 +5,10 @@ class AdminStaffController extends AdminComponent{
   public $model_class = 'Staff';
   public $display_name = "Staff";
   public $dashboard = false;
+  public $filter_fields=array(
+                          'text' => array('columns'=>array('id', 'title', 'content'), 'partial'=>'_filters_text', 'fuzzy'=>true),
+                          'departments' => array('columns'=>array('departments'), 'partial'=>'_filters_select', 'opposite_join_column'=>'staff'),
+                          'organisations' => array('columns'=>array('organisations'), 'partial'=>'_filters_select', 'opposite_join_column'=>'staff')
+                        );
 }
 ?>
