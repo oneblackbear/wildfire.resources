ed22c9a1c6e8970c1668d25d1602b0126d431afe
charlesmarshall <cm56marshall@googlemail.com>
2012-04-17 10:42:03 +0100

    working on the base level events

--- resources/app/controller/BaseController.php
+++ resources/app/controller/BaseController.php
@@ -2,7 +2,9 @@
 class BaseController extends WaxController{
 
   public $model_class = false;
+  public $model_scope = "live";
   public $staff = false;
+  public $per_page = 15;
   public $permissions = array(
                           'create'=>array('owner', 'admin'),
                           'edit'=>array('owner', 'admin'),
@@ -38,9 +40,66 @@ class BaseController extends WaxController{
         if(!$controller->staff || !in_array($controller->staff->role, $roles)) $controller->redirect_to($controller->base_url()."?no-access");
       }
     });
+    /**
+     * filter code nicked from cms controller
+     */
+    WaxEvent::add("model.filters", function(){
+      $obj = WaxEvent::data();
+      if(!$obj->model_filters) $obj->model_filters = Request::param('filters');
+      $filterstring = "";
+
+      foreach((array)$obj->model_filters as $name=>$value){
+        $col_filter = "";
+        if(strlen($value) && $filter = $obj->filter_fields[$name]){
+          foreach($filter['columns'] as $col){
+            if($opp = $filter['opposite_join_column']){
+              $target = $obj->model->columns[$col][1]['target_model'];
+              $join = new $target($value);
+              $ids = array();
+              foreach($join->$opp as $opposite) $ids[] = $opposite->primval;
+              $col_filter .= "(`".$obj->model->primary_key."` IN(".implode(",",$ids).")) OR";
+            }
+            elseif($filter['fuzzy']) $col_filter .= "`$col` LIKE '%".($value)."%' OR";
+            elseif($filter['fuzzy_right']) $col_filter .= "`$col` LIKE '".($value)."%' OR";
+            elseif($filter['fuzzy_left']) $col_filter .= "`$col` LIKE '%".($value)."' OR";
+            else $col_filter .= "`$col`='".($value)."' OR";
+          }
+          $filterstring .= "(".trim($col_filter, " OR").") AND ";
+        }
+      }
+
+      if($filterstring) $obj->model->filter(trim($filterstring, " AND "));
+    });
+    /**
+     * setup the pagination on the model
+     */
+    WaxEvent::add("model.pagination.setup", function(){
+      $obj = WaxEvent::data();
+      if(!Request::param('view_all')){
+        if(!$obj->this_page = Request::param('page')) $obj->this_page = 1;
+        if($per_page = Request::param('per_page')) $obj->per_page = $per_page;
+      }else $obj->this_page = $obj->per_page = false;
+    });
+    WaxEvent::add("model.fetch", function(){
+      $obj = WaxEvent::data();
+      if($obj->this_page && $obj->per_page) $obj->results = $obj->model->page($obj->this_page, $obj->per_page);
+      else $obj->results = $obj->model->all();
+    });
+
+    /**
+     * event to handle model setups
+     * - first looks for a valid id in the url (for /controller/edit/x/)
+     * - then looks for filters applied for this model (like a listing)
+     */
     WaxEvent::add('model.setup', function(){
       $controller = WaxEvent::data();
-
+      if($id = Request::get("id")) $controller->model = new $controller->model_class($id);
+      else{
+        $controller->model = new $controller->model_class($controller->model_scope);
+        if($controller->model_filters || Request::param('filters')) WaxEvent::run("model.filters", $controller);
+        WaxEvent::run("model.pagination.setup", $controller);
+        WaxEvent::run("model.fetch", $controller);
+      }
     });
   }
 
@@ -48,7 +107,6 @@ class BaseController extends WaxController{
 
   public function create(){
     WaxEvent::run("model.setup", $this);
-    WaxEvent::run("model.save", $this);
   }
 
   public function edit(){
