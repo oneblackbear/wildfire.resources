4fdd56cdda604a3a21aaff5dc4fba43fe48a56eb
charlesmarshall <cm56marshall@googlemail.com>
2012-04-17 08:55:28 +0100

    actually, a client is a lot like an organisation, so add a boolean and drop the client model

--- lib/model/Organisation.php
+++ lib/model/Organisation.php
@@ -2,8 +2,11 @@
 class Organisation extends WildfireResource{
   public function setup(){
     parent::setup();
+    $this->define("is_client", "BooleanField");
+    $this->define("account_handler", "ForeignKey", array('target_model'=>"Staff", 'group'=>'relationships', 'scaffold'=>true));
     $this->define("departments", "ManyToManyField", array('target_model'=>"Department", 'group'=>'relationships'));
     $this->define("staff", "ManyToManyField", array('target_model'=>"Staff", 'group'=>'relationships'));
+    $this->define("fee", "ForeignKey", array('target_model'=>"Fee", 'group'=>'relationships'));
   }
 
 
--- setup.php
+++ setup.php
@@ -3,10 +3,9 @@ CMSApplication::register_module("organisation", array('plugin_name'=>'wildfire.r
 CMSApplication::register_module("department", array("display_name"=>"Departments", "link"=>"/admin/department/"));
 CMSApplication::register_module("staff", array("display_name"=>"Staff", "link"=>"/admin/staff/", 'split'=>true));
 
-CMSApplication::register_module("client", array("display_name"=>"Clients", "link"=>"/admin/client/"));
-CMSApplication::register_module("fee", array("display_name"=>"Fees", "link"=>"/admin/fee/",'split'=>true));
-
 CMSApplication::register_module("job", array("display_name"=>"Jobs", "link"=>"/admin/job/"));
 CMSApplication::register_module("work", array("display_name"=>"Schedule", "link"=>"/admin/work/"));
 CMSApplication::register_module("jobcomment", array("display_name"=>"Comments on jobs", "link"=>"/admin/jobcomment/", 'split'=>true));
+
+CMSApplication::register_module("fee", array("display_name"=>"Fees", "link"=>"/admin/fee/",'split'=>true));
 ?>
