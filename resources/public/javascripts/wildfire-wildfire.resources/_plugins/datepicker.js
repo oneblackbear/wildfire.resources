ae78d8fed32e4a48225fd3c9e577881c42cf4c25
charlesmarshall <cm56marshall@googlemail.com>
2012-05-03 12:13:03 +0100

    date format

--- resources/public/javascripts/wildfire-wildfire.resources/_plugins/datepicker.js
+++ resources/public/javascripts/wildfire-wildfire.resources/_plugins/datepicker.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function(){
 
   jQuery(window).bind("datepicker", function(){
-    jQuery('.date_field').datepicker({changeMonth: true, changeYear:true});
+    jQuery('.date_field').datepicker({dateFormat:"d MM yy", changeMonth: true, changeYear:true});
   });
 
   jQuery(window).trigger("datepicker");
