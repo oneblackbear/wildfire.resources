b79e42fe236b67db6bac4bb0d030fb900b5c691b
charlesmarshall <cm56marshall@googlemail.com>
2012-05-09 16:44:02 +0100

    follow me tab

--- resources/public/javascripts/wildfire-wildfire.resources/_plugins/_help_tab.js
+++ resources/public/javascripts/wildfire-wildfire.resources/_plugins/_help_tab.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function(){
-
+  var start_pos = jQuery(".help_tab").offset().top;
   jQuery(".help_tab .help.button").on("click", function(e){
     e.preventDefault();
     var obj = jQuery(this),
@@ -17,5 +17,15 @@ jQuery(document).ready(function(){
     //escape
     if(e.which == 27) jQuery(".help_tab .button").trigger("click");
   });
+  jQuery(window).on("resize", function(){
+    jQuery(".help_tab .inside").css({"height":jQuery(window).height()-(start_pos), "overflow":"auto"});
+  });
+
+  jQuery(document).scroll(function(e){
+    jQuery(document).scrollTop();
+    if(jQuery(document).scrollTop() > start_pos) jQuery(".help_tab").css({"position":"fixed", "top":0});
+    else jQuery(".help_tab").css({"position":"absolute", "top":""});
+  });
+  jQuery(window).trigger("resize");
 
 });
