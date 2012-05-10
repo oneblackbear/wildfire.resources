7461a7d337b37611ab4112a8b91f4c8f088cb035
charlesmarshall <cm56marshall@googlemail.com>
2012-05-10 14:41:51 +0100

    add in client filter on calendar

--- resources/app/controller/WorkController.php
+++ resources/app/controller/WorkController.php
@@ -6,7 +6,8 @@ class WorkController extends BaseController{
   public $filter_fields=array(
                           'department' => array('columns'=>array('department'), 'partial'=>'_filters_select', 'opposite_join_column'=>'work'),
                           'staff' => array('columns'=>array('staff'), 'partial'=>'_filters_select', 'opposite_join_column'=>'work'),
-                          'job' => array('columns'=>array('job'), 'partial'=>'_filters_select', 'opposite_join_column'=>'work')
+                          'job' => array('columns'=>array('job'), 'partial'=>'_filters_select', 'opposite_join_column'=>'work'),
+                          'client' => array('columns'=>array('client'), 'partial'=>'_filters_select', 'opposite_join_column'=>'work')
                         );
   public $navigation_links = array('index', 'listing', 'to_do', 'graphs');
   public $permissions = array(
