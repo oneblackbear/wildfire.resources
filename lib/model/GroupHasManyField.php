a3f25cd09ee42922fc0e25203efb9f6ce2d2b6b1
charlesmarshall <cm56marshall@googlemail.com>
2012-05-10 11:38:00 +0100

    custom has many join to filter by group

---
+++ lib/model/GroupHasManyField.php
@@ -0,0 +1,36 @@
+<?php
+
+class GroupHasManyField extends HasManyField {
+
+  public $default_scope = "live";
+
+  public function get($filters = false) {
+    $target = new $this->target_model($this->default_scope);
+    if($filters) $target->filter($filters);
+    if($this->join_order) $target->order($this->join_order);
+    if($this->eager_loading) return $this->eager_load($target);
+    return $this->lazy_load($target);
+  }
+
+  public function eager_load($target) {
+		$cache = WaxModel::get_cache($this->target_model.":".md5(serialize($target->filters)), $this->field, $this->model->primval, false);
+		if(is_array($cache)) return new WaxModelAssociation($this->model, $target, $cache, $this->field);
+    $vals = $target->filter(array($this->join_field=>$this->model->primval))->all();
+		WaxModel::set_cache($this->target_model.":".md5(serialize($target->filters)), $this->field, $this->model->primval, $vals->rowset);
+		return new WaxModelAssociation($this->model->scope($this->default_scope)->filter("group_token", Session::get("GROUP")), $target, $vals->rowset, $this->field);
+  }
+
+  public function lazy_load($target) {
+    $target->filter(array($this->join_field=>$this->model->primval));
+    foreach($target->rows() as $row) $ids[]=$row[$target->primary_key];
+    return new WaxModelAssociation($this->model->scope($this->default_scope)->filter("group_token", Session::get("GROUP")), $target, $ids, $this->field);
+  }
+
+  public function get_choices() {
+    $j = new $this->target_model($this->default_scope);
+    if($this->identifier) $j->identifier = $this->identifier;
+    foreach($j->filter("group_token", Session::get("GROUP"))->all() as $row) $this->choices[$row->{$row->primary_key}]=$row->{$row->identifier};
+    return $this->choices;
+  }
+}
+?>
