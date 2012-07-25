72f4702f02cdd600bf84f0f23ac403858c51c2e7
charlesmarshall <cm56marshall@googlemail.com>
2012-07-25 10:47:51 +0100

    more per page

--- resources/app/controller/ResponsibilityController.php
+++ resources/app/controller/ResponsibilityController.php
@@ -2,6 +2,7 @@
 class ResponsibilityController extends BaseController{
   public $model_class = "Responsibility";
   public $form_name = "responsibility_form";
+  public $per_page = 100;
   public $filter_fields=array(
                           'staff' => array('columns'=>array('staff'), 'partial'=>'_filters_select', 'opposite_join_column'=>'work'),
                           'client' => array('columns'=>array('client'), 'partial'=>'_filters_select', 'opposite_join_column'=>'work'),
