883b0c125674e8fd5741c21c83bf0686f58a669c
charlesmarshall <cm56marshall@googlemail.com>
2012-07-25 09:41:16 +0100

    extend other model

--- lib/model/Responsibility.php
+++ lib/model/Responsibility.php
@@ -1,9 +1,11 @@
 <?
-class Responsibility extends WaxModel{
+class Responsibility extends WildfireResource{
 
   public static $scope_cache = array();
   public function setup(){
     $this->columns['id'][1]['scaffold']=true;
+    $this->columns['title'][1]['editable']=false;
+    $this->columns['content'][1]['editable']=false;
     $this->define("client", "ForeignKey", array('target_model'=>"Organisation", 'group'=>'allocations', 'scaffold'=>true, 'eager_load'=>false));
     $this->define("staff", "ManyToManyField", array('target_model'=>"Staff", 'group'=>'relationships'));
   }
