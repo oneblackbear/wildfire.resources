85542a475fb7e02a48f2b1a21dd0634b55c3f992
charlesmarshall <cm56marshall@googlemail.com>
2012-07-25 09:44:49 +0100

    fix the model

--- lib/model/Responsibility.php
+++ lib/model/Responsibility.php
@@ -3,11 +3,13 @@ class Responsibility extends WildfireResource{
 
   public static $scope_cache = array();
   public function setup(){
+    parent::setup();
     $this->columns['id'][1]['scaffold']=true;
     $this->columns['title'][1]['editable']=false;
+    $this->columns['title'][1]['scaffold']=false;
     $this->columns['content'][1]['editable']=false;
     $this->define("client", "ForeignKey", array('target_model'=>"Organisation", 'group'=>'allocations', 'scaffold'=>true, 'eager_load'=>false));
-    $this->define("staff", "ManyToManyField", array('target_model'=>"Staff", 'group'=>'relationships'));
+    $this->define("staff", "ManyToManyField", array('target_model'=>"Staff", 'group'=>'relationships', 'scaffold'=>true));
   }
 
 
