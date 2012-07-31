89bc6823d0bc1d771c1c488156253ea7011f373d
charlesmarshall <cm56marshall@googlemail.com>
2012-07-31 11:59:15 +0100

    dont show dead jobs

--- resources/app/controller/WorkController.php
+++ resources/app/controller/WorkController.php
@@ -60,7 +60,7 @@ class WorkController extends BaseController{
 
       //find all jobs within this time period as well
       $job = new Job;
-      foreach($job->for_department($this->active_staff->department_id())->all() as $row){
+      foreach($job->filter("dead", 0)->for_department($this->active_staff->department_id())->all() as $row){
         $this->calendar_content["j".$row->primval] = $row;
         $end = date("Y-m-d", strtotime($row->date_go_live));
         $this->month_events[$end]["j".$row->primval] = $row->primval;
